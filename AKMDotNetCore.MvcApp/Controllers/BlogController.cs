40602c24aacef9e1640e225f99ac5413e428c4a1
aungkaungmyat <aungkaungmyatyeu123@gmail.com>
2023-11-29 19:22:56 +0630

    [Complete] Mvc Read

---
+++ AKMDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using AKMDotNetCore.MvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AKMDotNetCore.MvcApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        [ActionName("Index")]
+        public IActionResult BlogIndex()
+        {
+            AppDbContext db = new AppDbContext();
+            List<BlogDataModel> lst = db.Blogs.ToList();
+            return View("BlogIndex", lst);
+        }
+    }
+}
